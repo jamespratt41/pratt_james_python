900097c5b172aa87ccd347f71bb7e4008cfd2d6e
James Pratt <james.r.pratt41@gmail.com>
2018-10-28 20:43:10 -0400

    Added finished files

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # pratt_james_python
+Based on the starting point, I added the player an computer lives.
+The reduction code was easy to write, as all I had to do was reduce the lives every time a player lost.
+I originally used the syntax "lives--", this was an error i couldn't figure out for awhile.
+I just reverted the syntax to "lives = lives - 1" and this fixed my problems.
+
+The largest issue I was having was how to hide the player choices after a player had lost all of their lives.
+This was resolved by using a endGame variable.
+When a player runs out of lives, the endGame variable was called, then not displays the choices.
+
+I have written this game before in PHP, the transfer to python was a challenge as for the change in structure and syntax.
+Aside from that, this game was a fun challenge.
--- RPSGame.py
+++ RPSGame.py
@@ -2,10 +2,108 @@ from random import randint
 
 #available weapons - this is stored in an array
 choices = ["Rock", "Paper", "Scissors"]
+player = False
+endGame = False
 
-#make the computer pick an item at random
+# resets the lives to three when start has not been pressed
+start = False
+if start == False:
+    computerLives = 3
+    playerLives = 3
 
-computer_choice = choices[randint(0,2)]
 
-#print the text along with the computers choice to the screen
-print("Computer chooses: ", computer_choice)
+print("Type any key to start game")
+start = input()
+
+# checks to see if the player has initiated the game
+while start != False:
+
+    # Checks to see if the player has made an input
+    while player == False:
+
+        #make the computer pick an item at random
+        computer_choice = choices[randint(0,2)]
+
+
+        #print the text along with the computers choice to the screen
+        if endGame == False:
+            # prints the amaount of lives each player has and what the computer has chosen.
+            print("\nComputer has ", computerLives, " lives.")
+            print("Player has ", playerLives, " lives.")
+            print("Computer chooses:", computer_choice)
+            # prints the player choices and their choice
+            print('Choose your weapon')
+            player = input("Rock, Paper, or Scissors?\n")
+            print("player chooses:", player)
+
+        if endGame == True:
+            player = input()
+
+        if(player == computer_choice):
+            print("TIE!")
+
+        elif(player == "Rock"):
+            if(computer_choice == "Paper"):
+                print("Paper covers Rock")
+                playerLives = playerLives -1
+                # end game check. Also resets game
+                if playerLives== 0:
+                    print("YOU LOSE! TRY HARDER NEXT TIME! \n Type 'Quit' to leave, or 'Reset' to play again.")
+                    endGame = True
+
+            if(computer_choice == "Scissors"):
+                print("Rock smashes Scissors")
+                computerLives = computerLives -1
+                if computerLives== 0:
+                    print("YOU WIN! YOU'RE REALLY COOL! \n Type 'Quit' to leave, or 'Reset' to play again.")
+                    endGame = True
+
+        elif(player == "Paper"):
+            if(computer_choice == "Scissors"):
+                print("Scossors cut Paper")
+                playerLives = playerLives -1
+                # end game check. Also resets game
+                if playerLives== 0:
+                    print("YOU LOSE! TRY HARDER NEXT TIME! \n Type 'Quit' to leave, or 'Reset' to play again.")
+                    endGame = True
+
+            if(computer_choice == "Rock"):
+                print("Paper covers Rock")
+                computerLives = computerLives -1
+                if computerLives== 0:
+                    print("YOU WIN! YOU'RE REALLY COOL! \n Type 'Quit' to leave, or 'Reset' to play again.")
+                    endGame = True
+
+        elif(player == "Scissors"):
+            if(computer_choice == "Rock"):
+                print("Rock Smashes Scissors")
+                playerLives = playerLives -1
+                # end game check. Also resets game
+                if playerLives== 0:
+                    print("YOU LOSE! TRY HARDER NEXT TIME! \n Type 'Quit' to leave, or 'Reset' to play again.")
+                    endGame = True
+
+            if(computer_choice == "Paper"):
+                print("Scissors cut Paper")
+                computerLives = computerLives -1
+                if computerLives== 0:
+                    print("YOU WIN! YOU'RE REALLY COOL! \n Type 'Quit' to leave, or 'Reset' to play again.")
+                    endGame = True
+
+        # if player types quit, the game will end
+        elif player == "Quit":
+            exit()
+
+        # if the player types reset, the lives are restored and the enGame variable is reset
+        elif player == "Reset":
+            computerLives = 3
+            playerLives = 3
+            endGame = False
+
+        else:
+            print("Not a valid input")
+
+
+
+    player = False
+    computer_choice = choices[randint(0,2)]
