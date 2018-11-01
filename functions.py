639119e4344fc2946d1d6f3c6cb45fc66b072a0b
James Pratt <james.r.pratt41@gmail.com>
2018-11-01 15:01:04 -0400

    added functions file

---
+++ functions.py
@@ -0,0 +1,15 @@
+# this is to teach me how to do functions yo
+
+def greeting():
+    #say hey
+    print("hey ya dirty lil boi")
+
+# how to envoke functions
+
+
+greeting()
+
+def greetings(msg):
+    print("Our function says", msg)
+
+greetings("This is an argument")
